6acdecf7ff3573e5be9d1d3b1097e9d94de64b88
Skylar <skylar.som@gmail.com>
2021-11-28 00:00:38 -0500

    Fix TextComponentCodec to properly use JSON

--- src/main/java/xyz/brassgoggledcoders/advisor/codec/TextComponentCodec.java
+++ src/main/java/xyz/brassgoggledcoders/advisor/codec/TextComponentCodec.java
@@ -1,39 +1,36 @@
 package xyz.brassgoggledcoders.advisor.codec;
 
+import com.google.gson.JsonElement;
 import com.google.gson.JsonParseException;
 import com.mojang.datafixers.util.Pair;
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.DataResult;
 import com.mojang.serialization.DynamicOps;
+import com.mojang.serialization.JsonOps;
 import net.minecraft.util.text.ITextComponent;
-
-import java.util.Objects;
 
 public class TextComponentCodec implements Codec<ITextComponent> {
     public static final Codec<ITextComponent> CODEC = new TextComponentCodec();
 
     @Override
     public <T> DataResult<Pair<ITextComponent, T>> decode(DynamicOps<T> ops, T input) {
-        return ops.getMap(input)
-                .map(Objects::toString)
-                .flatMap(string -> {
-                    try {
-                        string = string.substring(8, string.length() - 1);
-                        return DataResult.success(Pair.of(
-                                ITextComponent.Serializer.fromJson(string),
-                                ops.empty())
-                        );
-                    } catch (JsonParseException e) {
-                        return DataResult.error(e.getMessage());
-                    }
-                });
+        JsonElement jsonElement = ops.convertTo(JsonOps.INSTANCE, input);
+
+        try {
+            return DataResult.success(Pair.of(
+                    ITextComponent.Serializer.fromJson(jsonElement),
+                    ops.empty()
+            ));
+        } catch (JsonParseException e) {
+            return DataResult.error(e.getMessage());
+        }
     }
 
     @Override
     public <T> DataResult<T> encode(ITextComponent input, DynamicOps<T> ops, T prefix) {
         try {
-            String text = ITextComponent.Serializer.toJson(input);
-            return DataResult.success(ops.createString(text));
+            JsonElement jsonElement = ITextComponent.Serializer.toJsonTree(input);
+            return DataResult.success(JsonOps.INSTANCE.convertTo(ops, jsonElement));
         } catch (JsonParseException e) {
             return DataResult.error(e.getMessage());
         }
